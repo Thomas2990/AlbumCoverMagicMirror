367a649f354b84ba8c346ed93128b9211bc6e2bc
Thomas2990 <42387070+Thomas2990@users.noreply.github.com>
2020-08-01 17:23:51 -0500

    Added Next and Previous songs

--- modules/MMM-NowPlayingOnSpotify/core/SpotifyConnector.js
+++ modules/MMM-NowPlayingOnSpotify/core/SpotifyConnector.js
@@ -5,7 +5,8 @@ const moment = require('moment');
 
 const tokenRefreshEndpoint = 'https://accounts.spotify.com/api/token';
 const apiEndpoint = 'https://api.spotify.com/v1/me/player';
-
+const nextSongEndpoint = 'https://api.spotify.com/v1/me/player/next';
+const previousSongEndpoint = 'https://api.spotify.com/v1/me/player/previous';
 
 module.exports = class SpotifyConnector {
 
@@ -35,6 +36,33 @@ module.exports = class SpotifyConnector {
         });
     }
   }
+  
+  requestNextSong() {
+    /*if (moment().isBefore(this.tokenExpiresAt)) {
+      return this.nextSong();
+
+    } else {
+      return this.refreshAccessToken()
+        .then((response) => {
+          console.log('Refreshed access token because it has expired. Expired at: %s now is: %s',
+            this.tokenExpiresAt.format('HH:mm:ss'), moment().format('HH:mm:ss'));
+
+          this.credentials.accessToken = response.access_token;
+          this.tokenExpiresAt = moment().add(response.expires_in, 'seconds');
+
+          return this.nextSong();
+        })
+        .catch((err) => {
+          console.error('Error while refreshing:');
+          console.error(err);
+        });
+    } MIGHT CAUSE A DOUBLE REQUEST FOR TOKEN, TAKING OUT FOR NOW*/
+    return this.nextSong();
+  }
+  
+  requestPreviousSong() {
+    return this.previousSong();
+  }
 
   getSpotifyData() {
     let options = {
@@ -44,6 +72,24 @@ module.exports = class SpotifyConnector {
     };
 
     return request.get(options);
+  }
+  
+  nextSong() {
+    let options = {
+      url: nextSongEndpoint,
+      headers: {'Authorization': 'Bearer ' + this.credentials.accessToken},
+      json: true
+    };
+    return request.post(options);
+  }
+  
+  previousSong() {
+    let options = {
+      url: previousSongEndpoint,
+      headers: {'Authorization': 'Bearer ' + this.credentials.accessToken},
+      json: true
+    };
+    return request.post(options);
   }
 
   refreshAccessToken() {
